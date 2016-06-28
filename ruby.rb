5e8e3782e5371d88dcb17e932d4f5d8b6effdac3
same1507 <same1507@gmail.com>
2016-06-28 17:33:04 -0500

    Intro de Ruby

---
+++ ruby.rb
@@ -0,0 +1,33 @@
+#3.times { puts "Hello world "}
+
+#Ruby 2 space identation for each nested level is encouraged
+
+# example
+#if a+2
+
+#    b+3
+
+#end
+
+#*
+
+#puts impreme en consola
+
+#puts 3 devuelve nil por que su retorno no esta -> objeto
+
+#p 3 muestra la variable lo que tenga ahí -> !objeto
+
+
+=begin
+[...
+
+
+...]
+=end
+
+puts "Ingrese un numero"
+a = gets.chomp
+a = a.to_i
+b = a+1
+
+puts b
